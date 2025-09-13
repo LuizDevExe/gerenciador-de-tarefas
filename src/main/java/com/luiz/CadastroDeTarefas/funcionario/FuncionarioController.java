4988979819ad581cc42da19031602d3bb0e8a704
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-09-13 03:08:48 -0300

    feat: Estrutura e inserção da rota de listagem dos funcionários

--- src/main/java/com/luiz/CadastroDeTarefas/funcionario/FuncionarioController.java
+++ src/main/java/com/luiz/CadastroDeTarefas/funcionario/FuncionarioController.java
@@ -5,12 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/funcionarios")
 public class FuncionarioController {
+
+    private FuncionarioService funcionarioService;
+
+    public FuncionarioController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Boas vindas";
+    }
+
+    @GetMapping("/listar")
+    public List<FuncionarioModel> listarFuncionarios(){
+        return funcionarioService.listarFuncionarios();
     }
 }
---
+++ src/main/java/com/luiz/CadastroDeTarefas/funcionario/FuncionarioService.java
@@ -0,0 +1,19 @@
+package com.luiz.CadastroDeTarefas.funcionario;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FuncionarioService {
+
+    private FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    public List<FuncionarioModel> listarFuncionarios() {
+        return funcionarioRepository.findAll();
+    }
+}

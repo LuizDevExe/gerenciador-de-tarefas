7230b0f3f5d7d6c8061eff1ec1f5d0f997ef7298
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-09-13 02:54:10 -0300

    fix: ajuste no lombok da entidade Tarefa, inicialização de contrutores, getters e setters

--- src/main/java/com/luiz/CadastroDeTarefas/tarefa/TarefaModel.java
+++ src/main/java/com/luiz/CadastroDeTarefas/tarefa/TarefaModel.java
@@ -3,12 +3,19 @@ package com.luiz.CadastroDeTarefas.tarefa;
 
 import com.luiz.CadastroDeTarefas.funcionario.FuncionarioModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "tb_tarefa")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TarefaModel {
 
     @Id
